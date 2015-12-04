9aa34904f78d83970f78e680d924430ba8fa368d
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-12-04 16:55:43 +0900

    popular palette api with client script

--- src/components/project/Generate.js
+++ src/components/project/Generate.js
@@ -1,11 +1,35 @@
 var React = require('react')
 var reactRouter = require('react-router')
 var Link = reactRouter.Link
+var whatwgFetch = require('whatwg-fetch')
 
 // Component
 var SentenceForm = require('../ui/SentenceForm.js')
+var Palette = require('../ui/Palette.js')
 
 var Generate = React.createClass({
+  getInitialState: function() {
+    return {
+      palette: []
+    };
+  },
+  componentWillMount: function() {
+    var self = this;
+    fetch('http://turkey.slis.tsukuba.ac.jp/~s1311495/api/v1/popular_palettes.php', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ZGI6dHN1a3ViYQ=='
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(response) {
+      if (response !== null) {
+        self.setState({palette: response})
+      }
+    })
+  },
   render() {
     return (
       <div className='container'>
@@ -28,8 +52,14 @@ var Generate = React.createClass({
             <h3>アプリ名</h3>
             <p>EmotionalPalette</p>
             <h3>開発者</h3>
-            <p>小林 正樹 (学籍番号:201311495)</p>
-            <h3>テーブル一覧(課題7)</h3>
+            <p>小林 正樹 : 学籍番号 201311495</p>
+            <h3>Source code</h3>
+            <p><a
+              href='https://github.com/makky3939/EmotionalPalette'
+              className='btn btn-primary-outline btn-sm'
+              target='_blank'
+            >GitHub</a></p>
+            <h3>テーブル一覧 : 課題7</h3>
             <p>
               <a
                 href='http://turkey.slis.tsukuba.ac.jp/~s1311495/sentence.php'
@@ -50,6 +80,20 @@ var Generate = React.createClass({
               >Favorite table</a>
             </p>
           </div>
+        </div>
+
+        <h2>Most popular palettes</h2>
+        <hr />
+        <div className='row'>
+          {
+            this.state.palette.map(function(item, index) {
+              return (
+                <div className="col-sm-4" key={index} >
+                  <Palette key={index} id={item.id} text={item.text} colors={item.colors} createdAt={item.createdAt}/>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )
