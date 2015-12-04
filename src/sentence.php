2f104b6e1089a5be759c052acd0ade6cd32709ae
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-12-04 12:51:58 +0900

    resolved task7

---
+++ src/sentence.php
@@ -0,0 +1,30 @@
+<?php
+  include './api/v1/_pdo.php';
+
+  try {
+    $pdo -> beginTransaction();
+    $statement = $pdo -> prepare('select * from sentence');
+    $statement -> execute();
+    $res = $statement -> fetchAll();
+    $pdo -> commit();
+  } catch (PDOException $e) {
+    exit('{"code": 400, "message": "bad request"}');
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang='jp'>
+  <head>
+    <meta charset='utf-8' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta name='viewport' content='width=device-width', 'initial-scale'='1' />
+    <title>EmotionalPalette</title>
+  </head>
+  <body>
+    <h1>Sentence table</h1>
+    <hr />
+    <pre><?php echo(var_dump($res)); ?></pre>
+    <hr />
+  </body>
+</html>
